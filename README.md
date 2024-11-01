a9e0b7e46896d112c3e98cf2cd0e1e8891c7ea9e
tompage1994@hotmail.co.uk <tpage@redhat.com>
2024-11-01 13:50:12 +0000

    Add auth and sysfile modules
    Signed-off-by: tompage1994@hotmail.co.uk <tpage@redhat.com>

--- README.md
+++ README.md
@@ -1,2 +1,87 @@
-# ansible-storage-protect
-Ansible collection for IBM Storage Protect
+# ibm.storage_protect
+
+## Description
+
+This collection provides a series of Ansible modules and plugins for interacting with the IBM Storage Protect product. For more information regarding this product, see [IBM Documentation](https://ibm.com/docs/en).
+
+
+## Requirements
+
+This collection requires the `dsmadmc` CLI to be installed on the target host.
+
+
+## Installation
+
+Before using this collection, you need to install it with the Ansible Galaxy command-line tool:
+
+```
+ansible-galaxy collection install ibm.storage_protect
+```
+
+You can also include it in a requirements.yml file and install it with ansible-galaxy collection install -r requirements.yml, using the format:
+
+
+```yaml
+collections:
+  - name: ibm.storage_protect
+```
+
+Note that if you install any collections from Ansible Galaxy, they will not be upgraded automatically when you upgrade the Ansible package.
+To upgrade the collection to the latest available version, run the following command:
+
+```
+ansible-galaxy collection install ibm.storage_protect --upgrade
+```
+
+You can also install a specific version of the collection, for example, if you need to downgrade when something is broken in the latest version (please report an issue in this repository). Use the following syntax to install version 1.0.0:
+
+```
+ansible-galaxy collection install ibm.storage_protect:==1.0.0
+```
+
+See [using Ansible collections](https://docs.ansible.com/ansible/devel/user_guide/collections_using.html) for more details.
+
+## Use Cases
+
+### Preparing the dsmadmc connection
+
+The dsmadmc CLI requires a `dsm.sys` file to be created to be able to point at IBM Storage Protect instance. The `sysfile` module exists to prepare this file in readiness for runing modules if this file has not already been creaed. You can use the following example code:
+
+```
+- name: Create dsm.sys
+  ibm.storage_protect.sysfile:
+    server_name: "ibmsp01"
+    tcp_server_address: "10.10.10.10"
+```
+
+### Managing resources in IBM Storage Protect
+
+Most of the modules in this collection exist to add, update or remove resources on the IBM Storage Protect instance. An example is adding (registering) a node. The following task will do this:
+
+```
+- name: Register hosts
+  ibm.storage_protect.node:
+    node: ibmsp01
+    policy_domain: STANDARD
+    schedules:
+      - my_sched
+    node_password: P@ssword123456789
+    session_security: strict
+    node_password_expiry: 90
+    can_archive_delete: true
+    min_extent_size: 250
+    state: present
+    server_name: "{{ ibmsp_host }}"
+    username: "{{ ibmsp_username }}"
+    password: "{{ ibmsp_password }}"
+```
+
+## Testing
+
+Tests should be defined in the `tests` repository of this collection. Here test playbooks should be created which can be run against an instance to ensure the modules are working correctly.
+
+
+## License Information
+Apache License, Version 2.0
+
+See [LICENSE](LICENSE) to see the full text.
---
+++ plugins/module_utils/storage_protect_api.py
@@ -0,0 +1,93 @@
+from __future__ import absolute_import, division, print_function
+
+__metaclass__ = type
+
+from ansible.module_utils.basic import AnsibleModule, env_fallback
+import subprocess
+
+
+class StorageProtectModule(AnsibleModule):
+    url = None
+    AUTH_ARGSPEC = dict(
+        server_name=dict(required=False, fallback=(env_fallback, ['SPECTRUM_PROTECT_SERVERNAME'])),
+        username=dict(required=False, fallback=(env_fallback, ['SPECTRUM_PROTECT_USERNAME'])),
+        password=dict(no_log=True, required=False, fallback=(env_fallback, ['SPECTRUM_PROTECT_PASSWORD'])),
+        request_timeout=dict(type='float', required=False, fallback=(env_fallback, ['SPECTRUM_PROTECT_REQUEST_TIMEOUT'])),
+    )
+    server_name = 'local'
+    username = None
+    password = None
+    validate_certs = True
+    request_timeout = 10
+    authenticated = False
+    version_checked = False
+    error_callback = None
+    warn_callback = None
+
+    def __init__(self, argument_spec=None, direct_params=None, error_callback=None, warn_callback=None, **kwargs):
+        full_argspec = {}
+        full_argspec.update(StorageProtectModule.AUTH_ARGSPEC)
+        full_argspec.update(argument_spec)
+        kwargs['supports_check_mode'] = True
+
+        self.error_callback = error_callback
+        self.warn_callback = warn_callback
+
+        self.json_output = {'changed': False}
+
+        if direct_params is not None:
+            self.params = direct_params
+        else:
+            super().__init__(argument_spec=full_argspec, **kwargs)
+
+        for param, _ in list(StorageProtectModule.AUTH_ARGSPEC.items()):
+            setattr(self, param, self.params.get(param))
+
+    def run_command(self, command, auto_exit=True, dataonly=True, exit_on_fail=True):
+        command = f'dsmadmc -servername={self.server_name} -id={self.username} -pass={self.password} ' + ('-dataonly=yes ' if dataonly else '') + command
+        self.json_output['command'] = command
+        try:
+            result = subprocess.run(command, shell=True, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            if auto_exit and result.returncode == 10:
+                self.json_output['changed'] = False
+                self.exit_json(**self.json_output)
+            if auto_exit and result.returncode == 0:
+                self.json_output['changed'] = True
+                self.json_output['output'] = result.stdout.decode('utf-8')
+                self.exit_json(**self.json_output)
+            return result.returncode, result.stdout.decode('utf-8'), None
+        except subprocess.CalledProcessError as e:
+            if auto_exit and e.returncode == 10:
+                self.json_output['changed'] = False
+                self.exit_json(**self.json_output)
+            if exit_on_fail and e.returncode != 10:
+                self.fail_json(msg=e.stdout.decode('utf-8'), rc=e.returncode, **self.json_output)
+            return e.returncode, e.stdout.decode('utf-8'), e
+
+    def find_one(self, object_type, name, fail_on_not_found=False):
+        command = f"-comma q {object_type} {name} format=detailed"
+        rc, out, _ = self.run_command(command, auto_exit=False, exit_on_fail=False)
+        exists = rc == 0
+        self.json_output['exists'] = exists
+        if fail_on_not_found and not exists:
+            self.fail_json(msg=f'Could not find {object_type} with name {name}')
+        return rc == 0, out
+
+    def perform_action(self, action, object_type, object_identifier, options='', exists=False, existing=None, auto_exit=True):
+        if not exists and action in ['remove', 'delete']:
+            if auto_exit:
+                self.exit_json(**self.json_output)
+            return 0
+        command = f"{action} {object_type} {object_identifier} {options}"
+        rc, output, error = self.run_command(command, auto_exit=False)
+        if exists or rc == 10:
+            # Check if idempotent
+            _, new_object = self.find_one(object_type, object_identifier)
+            self.json_output['changed'] = self.json_output['changed'] or existing and existing != new_object or exists and action in ['remove', 'delete']
+            if auto_exit:
+                self.exit_json(**self.json_output)
+            return rc
+        self.json_output['changed'] = True
+        if auto_exit:
+            self.exit_json(**self.json_output)
+        return rc
