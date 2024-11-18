927d77ee0e7eaad1fc74bcad879f9c3acb6cc6aa
Sarthak Kshirsagar <sarthak@Sarthaks-MacBook-Pro.local>
2024-11-18 11:25:24 +0530

    modified the command executor class so that it returns the rc code and added the success msg to indicate the status of BA installation for user
    Signed-off-by: Sarthak Kshirsagar <sarthak.ksh.dev@gmail.com>

--- plugins/modules/install_ba_client.py
+++ plugins/modules/install_ba_client.py
@@ -1,6 +1,6 @@
 from ansible.module_utils.basic import AnsibleModule
 from ansible.module_utils.command_executor import CommandExecutor
-from ansible.module_utils.install_ba_client_utils import CompatibilityChecker, SystemInfoCollector, extract_tar, install_rpm_packages_in_sequence, run_dsmc_command
+from ansible.module_utils.install_ba_client_utils import CompatibilityChecker, SystemInfoCollector, extract_tar, install_rpm_packages_in_sequence, parse_dsm_output
 import os
 
 def main():
@@ -21,13 +21,14 @@ def main():
         compatibility = compatibility_checker.check_compatibility()
         system_info['compatibility'] = compatibility
         result.update(system_info)
+        changed = False
 
         # If system is compatible, proceed with installation
         if result["compatibility"].get('compatible', False):
             try:
                 # Extract files and change to destination directory
                 extract_result = extract_tar(module, module.params['path'], module.params['dest_folder'])
-                result.update({"extract_result": extract_result})
+                # result.update({"extract_result": extract_result})
                 os.chdir(module.params['dest_folder'])
             except Exception as e:
                 module.fail_json(msg=f"Failed to extract or change directory: {str(e)}")
@@ -39,11 +40,15 @@ def main():
             except Exception as e:
                 module.fail_json(msg=f"Installation failed: {str(e)}")
 
+            dsmcOutput, rc = CommandExecutor.execute('sudo dsmc')
+            msg, changed = parse_dsm_output(dsmcOutput)
+            result.update({'dsmc output':msg})
+
 
         else:
             module.fail_json(msg="System compatibility check failed.", result=result)
 
-        module.exit_json(result=result)
+        module.exit_json(changed=changed,result=result)
     else:
         module.exit_json(msg="No commands executed, flag 'run_commands' is set to False.")
 
