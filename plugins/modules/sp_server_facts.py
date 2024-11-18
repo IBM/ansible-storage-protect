1b1fd659ea33d3e9fff3ff64193a69885c2d52b4
Sarthak Kshirsagar <sarthak@Sarthaks-MacBook-Pro.local>
2024-11-18 11:49:57 +0530

    Added the documentation and examples for the sp_server_facts module
    Signed-off-by: Sarthak Kshirsagar <sarthak@Sarthaks-MacBook-Pro.local>

--- plugins/modules/sp_server_facts.py
+++ plugins/modules/sp_server_facts.py
@@ -1,6 +1,105 @@
+#!/usr/bin/python
+# coding: utf-8 -*-
+
 from ansible.module_utils.dsmadmc_adapter import DsmadmcAdapter
 from ansible.module_utils.sp_server_facts import DSMParser,SpServerResponseMapper
 
+from plugins.modules.node import DOCUMENTATION
+
+DOCUMENTATION = '''
+---
+module: sp_server_facts
+author: "Sarthak Kshirsagar"
+short_description: Gather IBM Storage Protect Server facts
+description:
+    - This module gathers various server facts related to the IBM Storage Protect (SP) server using the DSMADMC command-line interface.
+    - It supports multiple queries such as server status, database, monitor settings, storage pools, and more.
+    - The output of each query is parsed and returned in a developer-friendly format.
+
+options:
+    server_name:
+        description: 
+            - The name or IP address of the IBM Storage Protect server to query.
+    username:
+        description: 
+            - The username to authenticate with the IBM Storage Protect server.
+    password:
+        description: 
+            - The password associated with the username for authentication.
+    q_status:
+        description:
+            - Whether to gather status information from the server.
+        type: bool
+        default: False
+    q_monitorsettings:
+        description:
+            - Whether to gather monitor settings information from the server.
+        type: bool
+        default: False
+    q_db:
+        description:
+            - Whether to gather database information from the server.
+        type: bool
+        default: False
+    q_dbspace:
+        description:
+            - Whether to gather database space information from the server.
+        type: bool
+        default: False
+    q_log:
+        description:
+            - Whether to gather log information from the server.
+        type: bool
+        default: False
+    q_domain:
+        description:
+            - Whether to gather domain information from the server.
+        type: bool
+        default: False
+    q_copygroup:
+        description:
+            - Whether to gather copygroup information from the server.
+        type: bool
+        default: False
+    q_replrule:
+        description:
+            - Whether to gather replication rule information from the server.
+        type: bool
+        default: False
+    q_devclass:
+        description:
+            - Whether to gather device class information from the server.
+        type: bool
+        default: False
+    q_mgmtclass:
+        description:
+            - Whether to gather management class information from the server.
+        type: bool
+        default: False
+    q_stgpool:
+        description:
+            - Whether to gather storage pool information from the server.
+        type: bool
+        default: False
+'''
+
+EXAMPLES = '''
+---
+- name: Gather server facts from IBM Storage Protect server
+  sp_server_facts:
+    server_name: "sp-server01"
+    username: "admin"
+    password: "password"
+    q_status: true
+    q_db: true
+    q_stgpool: true
+  register: result
+
+- name: Display results
+  debug:
+    var: result.results
+
+'''
 def main():
     argument_spec = dict(
         server_name=dict(type='str', required=False),
