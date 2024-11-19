fa5cc3f8628d92da0683307482fb3b7efad6424e
Sarthak Kshirsagar <sarthak@Sarthaks-MacBook-Pro.local>
2024-11-19 14:18:04 +0530

    added the functionality in module to just perform the compatibility check
    Signed-off-by: Sarthak Kshirsagar <sarthak@Sarthaks-MacBook-Pro.local>

--- plugins/module_utils/command_executor.py
+++ plugins/module_utils/command_executor.py
@@ -10,6 +10,6 @@ class CommandExecutor:
             result = subprocess.run(command, shell=True, text=True, capture_output=True)
 
             # Return both the command output and the return code
-            return result.stdout.strip(), result.returncode
+            return result.stdout.strip(), result.returncode, result.stderr.strip()
         except FileNotFoundError as e:
             return f"FileNotFoundError: {str(e)}", 1  # Non-zero return code on error
--- plugins/modules/install_ba_client.py
+++ plugins/modules/install_ba_client.py
@@ -7,8 +7,6 @@ from ansible.module_utils.command_executor import CommandExecutor
 from ansible.module_utils.install_ba_client_utils import CompatibilityChecker, SystemInfoCollector, extract_tar, install_rpm_packages_in_sequence, parse_dsm_output
 import os
 
-from plugins.modules.node import DOCUMENTATION
-
 # Module Documentation
 DOCUMENTATION = '''
 ---
@@ -38,10 +36,16 @@ options:
 
 EXAMPLES = '''
 ---
-- name: Copy folder to remote node
-      copy:
-        src: ./8.1.24.0-TIV-TSMBAC-LinuxX86.tar  # Path on the control node
-        dest: /tmp/8.1.24.0-TIV-TSMBAC-LinuxX86.tar
+- name: Copy the module on the remote host
+  ansible.builtin.synchronize:
+    src: "./8.1.24.0-TIV-TSMBAC-LinuxX86.tar"
+    dest: "/tmp/"
+    register: copy_result
+    
+- name: Check the system compatibility
+  install_ba_client:
+    install: false
+    register: compatible_check_result
 
 - name: Install IBM Storage Protect BA Client on remote system
   install_ba_client:
@@ -49,40 +53,43 @@ EXAMPLES = '''
     path: /tmp/8.1.24.0-TIV-TSMBAC-LinuxX86.tar
     dest_folder: /opt/baClient
   register: result
-
-- name: Skip installation if not required
-  install_ba_client:
-    install: false
-    path: /tmp/8.1.24.0-TIV-TSMBAC-LinuxX86.tar
-    dest_folder: /opt/baClient
 '''
+
+from ansible.module_utils.basic import AnsibleModule
+import os
+
 
 def main():
     module_args = dict(
         install=dict(type='bool', required=True),
-        path=dict(type='str', required=True),
-        dest_folder=dict(type='str', required=True),
+        path=dict(type='str', required=False),
+        dest_folder=dict(type='str', required=False),
     )
+
     module = AnsibleModule(argument_spec=module_args, supports_check_mode=False)
     result = {}
 
     if module.params['install']:
-        # Collecting system information and compatibility
-        command_executor = CommandExecutor()
-        system_info_collector = SystemInfoCollector(command_executor)
-        system_info = system_info_collector.collect()
-        compatibility_checker = CompatibilityChecker(system_info)
-        compatibility = compatibility_checker.check_compatibility()
-        system_info['compatibility'] = compatibility
-        result.update(system_info)
-        changed = False
+        if not module.params['path']:
+            module.fail_json(msg="The 'path' parameter is required when 'install' is set to True.")
+        if not module.params['dest_folder']:
+            module.fail_json(msg="The 'dest_folder' parameter is required when 'install' is set to True.")
 
+    command_executor = CommandExecutor()
+    system_info_collector = SystemInfoCollector(command_executor)
+    system_info = system_info_collector.collect()
+    compatibility_checker = CompatibilityChecker(system_info)
+    compatibility = compatibility_checker.check_compatibility()
+    system_info['compatibility'] = compatibility
+    result.update(system_info)
+    changed = False
+
+    if module.params['install']:
         # If system is compatible, proceed with installation
         if result["compatibility"].get('compatible', False):
             try:
                 # Extract files and change to destination directory
                 extract_result = extract_tar(module, module.params['path'], module.params['dest_folder'])
-                # result.update({"extract_result": extract_result})
                 os.chdir(module.params['dest_folder'])
             except Exception as e:
                 module.fail_json(msg=f"Failed to extract or change directory: {str(e)}")
@@ -94,19 +101,131 @@ def main():
             except Exception as e:
                 module.fail_json(msg=f"Installation failed: {str(e)}")
 
-            dsmcOutput, rc = CommandExecutor.execute('sudo dsmc')
-            msg, changed = parse_dsm_output(dsmcOutput)
-            result.update({'dsmc output':msg})
-
+            dsmc_output, rc, err = CommandExecutor.execute('sudo dsmc')
+            msg, changed = parse_dsm_output(dsmc_output)
+            result.update({'dsmc output': msg})
 
         else:
             module.fail_json(msg="System compatibility check failed.", result=result)
 
-        module.exit_json(changed=changed,result=result)
+        module.exit_json(changed=changed, result=result)
     else:
-        module.exit_json(msg="No commands executed, flag 'run_commands' is set to False.")
+        module.exit_json(msg="No commands executed, 'install' is set to False.", result=result)
+
 
 if __name__ == '__main__':
     main()
 
+# ======== working =======
+# def main():
+#     module_args = dict(
+#         install=dict(type='bool', required=True),
+#         path=dict(type='str', required=False),
+#         dest_folder=dict(type='str', required=False),
+#     )
+#     module = AnsibleModule(argument_spec=module_args, supports_check_mode=False)
+#     result = {}
+#
+#     command_executor = CommandExecutor()
+#     system_info_collector = SystemInfoCollector(command_executor)
+#     system_info = system_info_collector.collect()
+#     compatibility_checker = CompatibilityChecker(system_info)
+#     compatibility = compatibility_checker.check_compatibility()
+#     system_info['compatibility'] = compatibility
+#     result.update(system_info)
+#     changed = False
+#
+#     if module.params['install']:
+#         # Collecting system information and compatibility
+#         # command_executor = CommandExecutor()
+#         # system_info_collector = SystemInfoCollector(command_executor)
+#         # system_info = system_info_collector.collect()
+#         # compatibility_checker = CompatibilityChecker(system_info)
+#         # compatibility = compatibility_checker.check_compatibility()
+#         # system_info['compatibility'] = compatibility
+#         # result.update(system_info)
+#         # changed = False
+#
+#         # If system is compatible, proceed with installation
+#         if result["compatibility"].get('compatible', False):
+#             try:
+#                 # Extract files and change to destination directory
+#                 extract_result = extract_tar(module, module.params['path'], module.params['dest_folder'])
+#                 # result.update({"extract_result": extract_result})
+#                 os.chdir(module.params['dest_folder'])
+#             except Exception as e:
+#                 module.fail_json(msg=f"Failed to extract or change directory: {str(e)}")
+#
+#             # Identify and install RPM packages in sequence
+#             try:
+#                 install_results = install_rpm_packages_in_sequence(module.params['dest_folder'])
+#                 result.update({"install_results": install_results})
+#             except Exception as e:
+#                 module.fail_json(msg=f"Installation failed: {str(e)}")
+#
+#             dsmcOutput, rc = CommandExecutor.execute('sudo dsmc')
+#             msg, changed = parse_dsm_output(dsmcOutput)
+#             result.update({'dsmc output':msg})
+#
+#
+#         else:
+#             module.fail_json(msg="System compatibility check failed.", result=result)
+#
+#         module.exit_json(changed=changed,result=result)
+#     else:
+#         module.exit_json(msg="No commands executed, flag 'run_commands' is set to False.")
+#
+# if __name__ == '__main__':
+#     main()
+#
 
+
+# ======= not working ======== working
+# def main():
+#     module_args = dict(
+#         install=dict(type='bool', required=True),
+#         path=dict(type='str', required=False),
+#         dest_folder=dict(type='str', required=False),
+#     )
+#     module = AnsibleModule(argument_spec=module_args, supports_check_mode=False)
+#     result = {}
+#
+#     # Collect system information and check compatibility
+#     command_executor = CommandExecutor()
+#     system_info_collector = SystemInfoCollector(command_executor)
+#     system_info = system_info_collector.collect()
+#     compatibility_checker = CompatibilityChecker(system_info)
+#     compatibility = compatibility_checker.check_compatibility()
+#     system_info['compatibility'] = compatibility
+#     result.update(system_info)
+#
+#     if not compatibility.get('compatible', False):
+#         module.fail_json(msg="System compatibility check failed.", result=result)
+#
+#     if module.params['install']:
+#         # If system is compatible, proceed with extraction and installation
+#         try:
+#             # Extract files and change to destination directory
+#             extract_result = extract_tar(module, module.params['path'], module.params['dest_folder'])
+#             result.update(extract_result)
+#             os.chdir(module.params['dest_folder'])
+#         except Exception as e:
+#             module.fail_json(msg=f"Failed to extract or change directory: {str(e)}")
+#
+#         try:
+#             # Identify and install RPM packages in sequence
+#             install_results = install_rpm_packages_in_sequence(module.params['dest_folder'])
+#             result.update({"install_results": install_results})
+#         except Exception as e:
+#             module.fail_json(msg=f"Installation failed: {str(e)}")
+#
+#         dsmc_output, rc = CommandExecutor.execute('sudo dsmc')
+#         msg, changed = parse_dsm_output(dsmc_output)
+#         result.update({'dsmc_output': msg})
+#
+#         module.exit_json(changed=changed, result=result)
+#     else:
+#         module.exit_json(msg="System compatibility check passed. No installation performed.", result=result)
+#
+# if __name__ == '__main__':
+#     main()
