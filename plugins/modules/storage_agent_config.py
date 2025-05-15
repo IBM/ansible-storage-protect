9e15b13244cca654344ac3fa3de4adb30e2e8385
Sarthak Kshirsagar <sarthak.ksh.dev@gmail.com>
2025-05-15 19:57:52 +0530

    removed the usage of subprocess.run_module as it is not recommended
    Signed-off-by: Sarthak Kshirsagar <sarthak.ksh.dev@gmail.com>

--- plugins/modules/storage_agent_config.py
+++ plugins/modules/storage_agent_config.py
@@ -1,12 +1,11 @@
 from ansible.module_utils.basic import AnsibleModule
 import os
 from ..module_utils.dsmadmc_adapter import DsmadmcAdapter
-from ..module_utils.dsmc_adapter import DsmcAdapter
 
 EXAMPLES = '''
 ---
-- name: Configure Storage Agent on server side
-  hosts: server
+- name: Configure Storage Agent
+  hosts: all
   ibm.storage_protect.storage_agent_configure:
     stg_agent_name: true
     server_name: true
@@ -29,21 +28,7 @@ EXAMPLES = '''
     stg_agent_options_file_path: false
     server_tcp_port: false
     server_hl_address: false
-    server_password: false
-
-- name: Configure Storage Agent on client side
-  hosts: client
-  ibm.storage_protect.storage_agent_configure:
-    stg_agent_name: true
-    server_name: true
-    stg_agent_hl_add: false
-    lladdress: false
-    config_role: client
-    client_options_file_path: false
-    stg_agent_options_file_path: false
-    server_tcp_port: false
-    server_hl_address: false
-    server_password: false
+    ser_pass: false
 
 - name: Validate lanfree node
   hosts: all
@@ -55,153 +40,162 @@ EXAMPLES = '''
 '''
 
 def main():
+
     module_args = dict(
-        stg_agent_name=dict(type='str', required=True),
-        server_name=dict(type='str', required=True),
-        stg_agent_password=dict(type='str', required=False),
-        stg_agent_hl_add=dict(type='str', required=False),
-        lladdress=dict(type='str', required=False),
-        stg_agent_path_name=dict(type='str', required=False),
-        stg_agent_path_dest=dict(type='str', required=False),
-        library=dict(type='str', required=False),
-        device=dict(type='str', required=False),
-        copygroup_domain=dict(type='str', required=False),
-        copygroup_policyset=dict(type='str', required=False),
-        copygroup_mngclass=dict(type='str', required=False),
-        copygroup_destination=dict(type='str', required=False),
-        stg_pool=dict(type='str', required=False),
-        validate_lan_free=dict(type='bool', required=False, default=False),
-        node_name=dict(type='str', required=False),
-        config_role=dict(type='str', required=False, choices=['server', 'client']),
-        client_options_file_path=dict(type='str', required=True, default="/opt/tivoli/tsm/client/ba/bin/dsm.sys"),
-        stg_agent_options_file_path=dict(type='str', required=False, default='/opt/tivoli/tsm/StorageAgent/bin/dsmsta.opt'),
-        server_tcp_port=dict(type='str', required=False),
-        server_hl_address=dict(type='str', required=False),
-        server_password=dict(type='str', required=False),
-        server_ip=dict(type='str', required=False),
-    )
+            stg_agent_name=dict(type='str', required=True),
+            stg_agent_server_name=dict(type='str', required=False),
+            stg_agent_password=dict(type='str', required=False),
+            stg_agent_hl_add=dict(type='str', required=False),
+            lladdress=dict(type='str', required=False),
+            stg_agent_path_name=dict(type='str', required=False),
+            stg_agent_path_dest=dict(type='str', required=False),
+            library=dict(type='str', required=False),
+            device=dict(type='str', required=False),
+            copygroup_domain=dict(type='str', required=False),
+            copygroup_policyset=dict(type='str', required=False),
+            copygroup_mngclass=dict(type='str', required=False),
+            copygroup_destination=dict(type='str', required=False),
+            stg_pool=dict(type='str', required=False),
+            validate_lan_free=dict(type='bool', required=False, default=False),
+            node_name=dict(type='str', required=False),
+            client_options_file_path=dict(type='str', default="/opt/tivoli/tsm/client/ba/bin/dsm.sys"),
+            stg_agent_options_file_path=dict(type='str', default='/opt/tivoli/tsm/StorageAgent/bin/dsmsta.opt'),
+            stg_agent_bin_dir=dict(type='str', default='/opt/tivoli/tsm/StorageAgent/bin/'),
+            server_tcp_port=dict(type='str', required=False),
+            server_hl_address=dict(type='str', required=False),
+            ser_pass=dict(type='str', required=False),
+            start_stg_agent = dict(type=bool,default=True)
+        )
 
-    module = AnsibleModule(argument_spec=module_args, supports_check_mode=False)
+    module = AnsibleModule(argument_spec=module_args)
     params = module.params
-    changed = False
 
+    # tasks to be performed on the client
+    # 1. copy the dsmsta.opt.smp to dsmsta.opt required for setstorageserver command
+
+    rc,std_out,std_err = module.run_command('cp dsmsta.opt.smp dsmsta.opt', cwd=f"{params['stg_agent_bin_dir']}")
+    if rc:
+        module.fail_json(msg="Failed", std_err=std_err, std_out=std_out)
+
+    # steps to be performed on SP Server
     dsmadmc_adapter = DsmadmcAdapter(argument_spec=module_args)
-    dsmc_adapter = DsmcAdapter(argument_spec=module_args)
 
-    if not params['config_role'] and not params['validate_lan_free']:
-        module.fail_json(msg='Either config_role must be provided or validate_lan_free must be True')
-
-    # Validate LAN-Free
+    # Validate if a node is capable for LAN-Free data movement
     if params['validate_lan_free']:
         if not params['stg_agent_name'] or not params['node_name']:
             module.fail_json(msg="For LAN-Free validation, both stg_agent_name and node_name must be provided.")
+
+        rc, std_out, std_err = module.run_command(f"./dsmsta", cwd=f"{params['stg_agent_bin_dir']}")
         command = f"validate lanfree {params['node_name']} {params['stg_agent_name']}"
-        rc, std_out = dsmc_adapter.run_command(command)
-        if rc == 0:
-            changed = True
-        else:
-            module.fail_json(msg=f"Failed to validate LAN-Free data movement. Error: {std_out}")
+        rc, std_out = dsmadmc_adapter.run_command(command)
 
-    if params['config_role'] == 'server':
-        if not all([params['stg_agent_name'], params['stg_agent_password'], params['stg_agent_hl_add'],
-                    params['server_name'], params['server_password'], params['server_hl_address'], params['lladdress'],params['library'],
+    if not all([params['stg_agent_name'], params['stg_agent_password'], params['stg_agent_hl_add'],
+                    params['stg_agent_server_name'], params['ser_pass'], params['server_hl_address'], params['lladdress'],params['library'],
                     params['stg_agent_path_name'],params['stg_agent_path_dest'],params['device'],
-                    params['server_name'],params['server_password'],params['server_hl_address'],params['server_tcp_port'],
+                    params['stg_agent_server_name'],params['ser_pass'],params['server_hl_address'],params['server_tcp_port'],
                     params['copygroup_domain'],params['copygroup_policyset'],params['copygroup_mngclass'],params['stg_pool'],
+                    params['stg_agent_name'], params['stg_agent_password'], params['stg_agent_hl_add'],
+                     params['stg_agent_server_name'], params['ser_pass'], params['server_tcp_port']
                     ]):
-            module.fail_json(msg="Missing parameters for configuration.")
+        module.fail_json(msg="Please Provide all the parameters")
 
-        commands = []
+    commands = []
+    result = {}
         # storage -> server
-        define_server = (
+    define_server = (
             f"define server {params['stg_agent_name']} "
             f"serverpassword={params['stg_agent_password']} "
             f"hladdress={params['stg_agent_hl_add']} "
             f"lladdress={params['lladdress']} ssl=YES "
-        )
-        commands.append(define_server)
-        # path for storage agent
-        define_path = (
+    )
+    # path for storage agent
+    define_path = (
                 f"define path {params['stg_agent_name']} {params['stg_agent_path_name']} "
                 f"srctype=server desttype={params['stg_agent_path_dest']} "
                 f"library={params['library']} device={params['device']}"
             )
-        commands.append(define_path)
-        # define copygroup
-        define_copygroup = (
+    # define copygroup
+    define_copygroup = (
                 f"define copygroup {params['copygroup_domain']} {params['copygroup_policyset']} "
                 f"{params['copygroup_mngclass']} type=backup destination={params['stg_pool']}"
             )
-        commands.append(define_copygroup)
-        # activate policyset
-        activate_policy_set = f"activate policyset {params['copygroup_domain']} {params['copygroup_policyset']}"
-        commands.append(activate_policy_set)
+    activate_policy_set = f"activate policyset {params['copygroup_domain']} {params['copygroup_policyset']}"
 
-        for cmd in commands:
-            rc, std_out = dsmadmc_adapter.run_command(cmd)
-            if rc != 0:
-                module.fail_json(msg=f"Command failed: {cmd}\nError: {std_out}")
-        changed = True
+    commands.append(define_server)
+    commands.append(define_path)
+    commands.append(define_copygroup)
+    commands.append(activate_policy_set)
 
-        # setup server to server communication
-        server_to_server_communication = [
-            f"set servername {params['server_name']}",
-            f"set serverhladdress {params['server_ip']}",
-            f"set serverpassword {params['server_password']}",
+    for cmd in commands:
+        rc, std_out, std_err = dsmadmc_adapter.run_command(cmd,auto_exit=False)
+        result[cmd] = std_out
+        if rc!=10 and rc!=0:
+            module.fail_json(msg=f"Command failed here2: {cmd}\nError: {std_out}")
+
+    # setup server to server communication
+    server_to_server_communication = [
+            f"set servername {params['stg_agent_server_name']}",
+            f"set serverhladdress {params['server_hl_address']}",
+            f"set serverpassword {params['ser_pass']}",
             f"set serverlladdress {params['lladdress']}",
-        ]
+    ]
 
-        for cmd in server_to_server_communication:
-            rc, std_out = dsmadmc_adapter.run_command(cmd)
-            if rc != 0:
-                module.fail_json(msg=f"Command failed: {cmd}\nError: {std_out}")
-            changed = True
+    for cmd in server_to_server_communication:
+        rc, std_out, std_err = dsmadmc_adapter.run_command(cmd,auto_exit=False)
+        result[cmd] = std_out
+        if rc:
+            module.fail_json(msg=f"Command failed here: {cmd}\nError: {std_out}")
 
-    if params['config_role'] == 'client':
-        if not all([params['stg_agent_name'], params['stg_agent_password'], params['stg_agent_hl_add'],
-                    params['server_name'], params['server_password'], params['server_tcp_port']]):
-            module.fail_json(msg="Missing parameters for client configuration.")
 
-        setstorageserver_command = (
-            f"dsmsta setstorageserver myname={params['stg_agent_name']} "
-            f"mypassword={params['stg_agent_password']} myhladdress={params['stg_agent_hl_add']} "
-            f"servername={params['server_name']} serverpassword={params['server_password']} "
-            f"hladdress={params['server_hl_address']} lladdress={params['server_tcp_port']} ssl=yes"
-        )
+    setstorageserver_command = f"./dsmsta setstorageserver myname={params['stg_agent_name']} mypassword={params['stg_agent_password']} myhladdress={params['stg_agent_hl_add']} servername={params['stg_agent_server_name']} serverpassword={params['ser_pass']} hladdress={params['server_hl_address']} lladdress={params['server_tcp_port']} ssl=yes"
 
-        rc, std_out = dsmc_adapter.run_command(setstorageserver_command)
-        if rc != 0:
-            module.fail_json(msg=f"Failed to set storage server: {std_out}")
-        changed = True
+    rc, std_out, std_err = module.run_command(setstorageserver_command, cwd=f"{params['stg_agent_bin_dir']}")
+    if rc:
+        module.fail_json(msg="Failed", std_err=std_err, std_out=std_out)
+    result['cmd'] = std_out
 
-        # Modify dsmsta.opt
-        try:
-            opt_lines = [
+    # start stg agent
+    if params['start_stg_agent']:
+        rc, std_out, std_err = module.run_command(f"./dsmsta",cwd=f"{params['stg_agent_bin_dir']}")
+        if rc:
+            module.fail_json(msg="Failed to start the storage agent",std_err=std_err,std_out=std_out)
+        else:
+            result['stg_agent_started'] = True
+
+    try:
+        opt_lines = [
+                f"Servername {params['stg_agent_server_name']}"
                 f"COMMmethod         TCPip",
                 f"TCPPort            {params['server_tcp_port']}",
-                f"SSLPort            {params['server_tcp_port']}",
-                f"SSLadminPort       {params['lladdress']}",
+                f"SSLTCPPort            {params['server_tcp_port']}",
+                f"SSLTCPadminPort       {params['lladdress']}",
                 f"DEVCONFIG          devconfig.txt"
-            ]
-            with open(params['stg_agent_options_file_path'], 'w') as f:
-                f.write("\n".join(opt_lines) + "\n")
-        except Exception as e:
-            module.fail_json(msg=f"Failed to update dsmsta.opt: {str(e)}")
+        ]
+        os.makedirs(os.path.dirname(params['stg_agent_options_file_path']), exist_ok=True)
+        with open(params['stg_agent_options_file_path'], 'w') as f:
+            f.write("\n".join(opt_lines) + "\n")
+        changed = True
+    except Exception as e:
+        module.fail_json(msg=f"Failed to update dsmsta.opt: {str(e)}")
 
-        # modify dsm.sys file on client for lan-free backup
-        try:
-            dsm_sys_lines = [
-                f"Servername            {params['server_name']}",
+ # modify dsm.sys file on client for lan-free backup
+    try:
+        dsm_sys_lines = [
+                f"Servername           {params['stg_agent_server_name']}",
                 "LANfreeCOMMmethod     tcpip",
                 "enablelanfree         yes",
-                f"lanfreetcpserveraddress {params['server_ip']}",
-                f"lanfreetcpport        {params['lladdress']}"
-            ]
-            with open(params['client_options_file_path'], 'w') as f:
-                f.write("\n".join(dsm_sys_lines) + "\n")
-        except Exception as e:
-            module.fail_json(msg=f"Failed to update dsm.sys: {str(e)}")
-    module.exit_json(changed=changed, msg="Storage agent configuration completed successfully.")
+                f"lanfreetcpserveraddress {params['server_hl_address']}",
+                f"lanfreetcpport        {params['lladdress']}",
+                f"TCPServeraddress   {params['server_hl_address']}",
+        ]
+        os.makedirs(os.path.dirname(params['client_options_file_path']), exist_ok=True)
+        with open(params['client_options_file_path'], 'w') as f:
+            f.write("\n".join(dsm_sys_lines) + "\n")
+        changed = True
+    except Exception as e:
+        module.fail_json(msg=f"Failed to update dsm.sys: {str(e)}")
+
+    module.exit_json(msg="Configured",result=result)
 
 if __name__ == '__main__':
     main()
