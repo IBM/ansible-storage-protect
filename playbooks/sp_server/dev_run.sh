65bfaf87639839f4e8df43b8d008cf5757088e92
Shalu Mishra <Shalu.Mishra2@ibm.com>
2025-12-16 10:57:10 +0530

    SDDEVOPS-1452 Added repo path
    Signed-off-by: Shalu Mishra <Shalu.Mishra2@ibm.com>

--- playbooks/sp_server/dev_run.sh
+++ playbooks/sp_server/dev_run.sh
@@ -1,12 +1,12 @@
 # ansible-playbook playbook.yml -i inventory.ini \
-#   -e sp_source_module_dir=/mnt/c/Users/NikhilTanni/Downloads/Nikk-dev/IBM/ansible-storage-protect/plugins \
+#   -e sp_source_module_dir=/path/to/repo/IBM/ansible-storage-protect/plugins \
 #   --limit sp_server_linux
 
 ansible-playbook playbook.yml -i inventory.ini \
-  -e sp_source_module_dir=/mnt/c/Users/NikhilTanni/Downloads/Nikk-dev/IBM/ansible-storage-protect/plugins \
+  -e sp_source_module_dir=/path/to/repo/IBM/ansible-storage-protect/plugins \
   --limit sp_server_windows
 
 
 # ansible-playbook playbook_configure.yml -i inventory.ini \
-#   -e sp_source_module_dir=/mnt/c/Users/NikhilTanni/Downloads/Nikk-dev/IBM/ansible-storage-protect/plugins \
+#   -e sp_source_module_dir=/path/to/repo/IBM/ansible-storage-protect/plugins \
 #   --limit sp_server_linux
